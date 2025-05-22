7b3f44419876b25b04f404192f6d4bd73825964f
harshaln <harshaln369@gmail.com>
2025-05-23 01:11:14 +0530

    string compression

--- arrays-and-strings/16-string-compression.py
+++ arrays-and-strings/16-string-compression.py
@@ -1,2 +1,27 @@
-def string_compression(str):
-    pass
+"""
+String Compression: Implement a method to perform basic string compression using the counts
+of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the
+"compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+Hints: #92, # 11 0
+"""
+
+
+def string_compression(string):
+    if len(string) == 0:
+        return ""
+    cStr = string[0]
+    count = 1
+    for i in range(1, len(string)):
+        if string[i] == string[i - 1]:
+            count += 1
+        else:
+            cStr += str(count)
+            cStr += string[i]
+            count = 1
+    cStr += str(count)
+
+    return cStr
+
+
+print(string_compression("aabcccccaaad"))
